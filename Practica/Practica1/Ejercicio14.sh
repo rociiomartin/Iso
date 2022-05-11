45609583591a2b894bee97c5700b6094f7a4216a
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-05-11 15:11:18 -0300

    Create Ejercicio14.sh

---
+++ Practica/Practica1/Ejercicio14.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+<<Enunciado
+Renombrando Archivos: haga un script que renombre solo archivos de un directorio
+pasado como parametro agregandole una CADENA, contemplando las opciones:
+“-a CADENA”: renombra el fichero concatenando CADENA al final del nombre del
+archivo
+“-b CADENA”: renombra el fichero concantenado CADENA al principio del nombre
+del archivo
+Enunciado
+
+
